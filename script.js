33ed2f8c8056e5247e4681d96a8919394102d156
Dev-That <54503786+Dev-That@users.noreply.github.com>
2024-01-23 16:27:58 +0200

    Bugs fixes

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const mintButton = document.getElementById('mintButton');
 const tokenAmountInput = document.getElementById('tokenAmount');
 const minTokens = 21000;
 const rpcUrl = 'https://test-rpc-node-http.svmscan.io/';
-let web3 = new Web3(rpcUrl);
+let web3 = new Web3(Web3.givenProvider || rpcUrl);
 const decimals = 8;
 const savmchainId = 3110;
 
 window.ethereum.on('connect', () => {
-    getAccounts((accounts)=>{
+    getAccounts((accounts) => {
         if (accounts.length !== 0) {
             connectButton.disabled = true;
             mintButton.disabled = false;
         }
     })
-    
+
 })
 
 connectButton.addEventListener('click', () => {
@@ -53,7 +53,9 @@ function getTokenSupply() {
     tokenContract.methods.totalSupply().call()
         .then(supply => {
             // Convert to 8 decimals
-            const convertedValue = supply / (10n ** BigInt(decimals));
+            const supplyBN = new web3.utils.BN(supply); // Convert supply to a Big Number
+            const decimalsBN = new web3.utils.BN(10).pow(new web3.utils.BN(decimals)); // Calculate 10^decimals
+            const convertedValue = supplyBN.div(decimalsBN); // Divide supply by 10^decimals
             const supplySpan = document.getElementById('supply');
             supplySpan.textContent = convertedValue.toString();
 
@@ -63,26 +65,25 @@ function getTokenSupply() {
         });
 }
 
-function getAccounts(cb){
+function getAccounts(cb) {
     window.ethereum.request({ method: 'eth_accounts' }).then(cb);
 }
 
 
-
-
 mintButton.addEventListener('click', () => {
     const amount = tokenAmountInput.value;
-    const amountInTokenUnits = BigInt(amount) * (BigInt(10) ** BigInt(decimals));
+    const amountBN = new web3.utils.BN(amount); // Convert amount to a Big Number
+    const decimalsBN = new web3.utils.BN(10).pow(new web3.utils.BN(decimals)); // Calculate 10^decimals
+    const amountInTokenUnits = amountBN.mul(decimalsBN); // Multiply amount by 10^decimals
     getChain((correctChain) => {
         if (!correctChain) return alert('Wrong Chain!\nUse SatoshiVM Testnet');
         window.ethereum.request({ method: 'eth_accounts' })
             .then(accounts => {
-                console.log(tokenContract);
                 if (accounts.length === 0) {
                     console.error("No accounts found. Ensure MetaMask is connected.");
                     return;
                 }
-                return tokenContract.methods.mint(amountInTokenUnits.toString()).send({ from: accounts[0] });
+                tokenContract.methods.mint(amountInTokenUnits).send({ from: accounts[0] });
             })
             .then(result => {
                 console.log(`Minted ${amount} tokens.`);
